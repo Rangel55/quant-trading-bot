d9dfdb9247c8f3452ced13bb3173761d3593caba
Rangel55 <147571606+Rangel55@users.noreply.github.com>
2026-03-23 19:52:18 -0300

    refactor: binance.service.js - migra de Futures (fapi) para Spot (api/v3), remove leverage/margem

--- services/binance.service.js
+++ services/binance.service.js
@@ -6,104 +6,77 @@ const { API_KEY, API_SECRET, BASE_URL } = require('../config/settings');
 const http = axios.create({ timeout: 10000 });
 
 function sign(queryString) {
-        return crypto.createHmac('sha256', API_SECRET).update(queryString).digest('hex');
+          return crypto.createHmac('sha256', API_SECRET).update(queryString).digest('hex');
 }
 
 const headers = { 'X-MBX-APIKEY': API_KEY };
 
-// Retry automatico: tenta ate 3 vezes com 2s de espera entre tentativas
+// Retry automatico: tenta ate 3 vezes com 2s de espera
 async function withRetry(fn, tentativas) {
-        tentativas = tentativas || 3;
-        for (var i = 0; i < tentativas; i++) {
-                  try {
-                              return await fn();
-                  } catch (err) {
-                              if (i === tentativas - 1) throw err;
-                              console.warn('[RETRY] Tentativa ' + (i + 1) + ' falhou: ' + err.message + '. Tentando novamente em 2s...');
-                              await new Promise(function(r) { setTimeout(r, 2000); });
-                  }
-        }
+          tentativas = tentativas || 3;
+          for (var i = 0; i < tentativas; i++) {
+                      try {
+                                    return await fn();
+                      } catch (err) {
+                                    if (i === tentativas - 1) throw err;
+                                    console.warn('[RETRY] Tentativa ' + (i + 1) + ' falhou: ' + err.message + '. Tentando em 2s...');
+                                    await new Promise(function(r) { setTimeout(r, 2000); });
+                      }
+          }
 }
 
-// GET publico (sem autenticacao)
+// GET publico (sem autenticacao) - Spot
 async function publicGet(endpoint, params) {
-        params = params || {};
-        return withRetry(function() {
-                  return http.get(BASE_URL + endpoint, { params }).then(function(r) { return r.data; });
-        });
+          params = params || {};
+          return withRetry(function() {
+                      return http.get(BASE_URL + endpoint, { params }).then(function(r) { return r.data; });
+          });
 }
 
-// GET privado (com assinatura HMAC)
+// GET privado (com assinatura HMAC) - Spot
 async function privateGet(endpoint, params) {
-        params = params || {};
-        params.timestamp = Date.now();
-        var qs = new URLSearchParams(params).toString();
-        params.signature = sign(qs);
-        return withRetry(function() {
-                  return http.get(BASE_URL + endpoint, { params, headers }).then(function(r) { return r.data; });
-        });
+          params = params || {};
+          params.timestamp = Date.now();
+          var qs = new URLSearchParams(params).toString();
+          params.signature = sign(qs);
+          return withRetry(function() {
+                      return http.get(BASE_URL + endpoint, { params, headers }).then(function(r) { return r.data; });
+          });
 }
 
-// POST privado (com assinatura HMAC)
+// POST privado (com assinatura HMAC) - Spot
 async function privatePost(endpoint, params) {
-        params = params || {};
-        params.timestamp = Date.now();
-        var qs = new URLSearchParams(params).toString();
-        params.signature = sign(qs);
-        return withRetry(function() {
-                  return http.post(BASE_URL + endpoint, null, { params, headers }).then(function(r) { return r.data; });
-        });
+          params = params || {};
+          params.timestamp = Date.now();
+          var qs = new URLSearchParams(params).toString();
+          params.signature = sign(qs);
+          return withRetry(function() {
+                      return http.post(BASE_URL + endpoint, null, { params, headers }).then(function(r) { return r.data; });
+          });
 }
 
-// DELETE privado (cancelar ordens)
+// DELETE privado (cancelar ordens) - Spot
 async function privateDelete(endpoint, params) {
-        params = params || {};
-        params.timestamp = Date.now();
-        var qs = new URLSearchParams(params).toString();
-        params.signature = sign(qs);
-        return withRetry(function() {
-                  return http.delete(BASE_URL + endpoint, { params, headers }).then(function(r) { return r.data; });
-        });
+          params = params || {};
+          params.timestamp = Date.now();
+          var qs = new URLSearchParams(params).toString();
+          params.signature = sign(qs);
+          return withRetry(function() {
+                      return http.delete(BASE_URL + endpoint, { params, headers }).then(function(r) { return r.data; });
+          });
 }
 
-// Configura alavancagem para um simbolo
-async function setLeverage(symbol, leverage) {
-        try {
-                  const result = await privatePost('/fapi/v1/leverage', { symbol: symbol, leverage: leverage });
-                  console.log('Alavancagem configurada: ' + leverage + 'x em ' + symbol);
-                  return result;
-        } catch (err) {
-                  console.error('Erro ao configurar alavancagem:', err.message);
-                  throw err;
-        }
-}
-
-// Configura tipo de margem (ISOLATED ou CROSSED)
-// CORRIGIDO: nao relanca erro - apenas loga e continua para nao causar LIVE_ERROR
-async function setMarginType(symbol, marginType) {
-        try {
-                  await privatePost('/fapi/v1/marginType', { symbol: symbol, marginType: marginType });
-                  console.log('Margem configurada: ' + marginType + ' em ' + symbol);
-        } catch (err) {
-                  var code = err.response && err.response.data ? err.response.data.code : null;
-                  var msg = err.response && err.response.data ? err.response.data.msg : err.message;
-                  // -4046 = ja esta neste modo | outros erros tambem sao seguros de ignorar
-          console.log('setMarginType aviso [' + (code || 'ERR') + ']: ' + msg + ' - continuando...');
-                  // NAO relanca o erro: execucao continua para abertura da ordem
-        }
-}
-
-// Valida conexao com Binance Futures
+// Valida conexao com Binance Spot
 async function validateConnection() {
-        try {
-                  await publicGet('/fapi/v1/ping');
-                  const t = await publicGet('/fapi/v1/time');
-                  console.log('Binance Futures OK. Server time: ' + new Date(t.serverTime).toISOString());
-                  return true;
-        } catch (err) {
-                  console.error('Falha Binance Futures:', err.message);
-                  return false;
-        }
+          try {
+                      await publicGet('/api/v3/ping');
+                      const t = await publicGet('/api/v3/time');
+                      console.log('Binance Spot OK. Server time: ' + new Date(t.serverTime).toISOString());
+                      return true;
+          } catch (err) {
+                      console.error('Falha Binance Spot:', err.message);
+                      return false;
+          }
 }
 
-module.exports = { publicGet, privateGet, privatePost, privateDelete, setLeverage, setMarginType, validateConnection };
+module.exports = { publicGet, privateGet, privatePost, privateDelete, validateConnection };
