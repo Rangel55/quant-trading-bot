09d277641e4003a46ff529f4243826ca465bc516
Rangel55 <147571606+Rangel55@users.noreply.github.com>
2026-03-29 19:45:47 -0300

    Implement proxy support in Bybit service
    Added proxy support configuration for Axios requests.

--- services/bybit.service.js
+++ services/bybit.service.js
@@ -2,90 +2,113 @@ const axios = require('axios');
 const crypto = require('crypto');
 const { BYBIT_API_KEY, BYBIT_API_SECRET, BYBIT_BASE_URL } = require('../config/settings');
 
-const http = axios.create({ timeout: 10000 });
+// --- Suporte a Proxy (para operar fora do Brasil) ---
+// Configure no .env: PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS
+function buildAxiosConfig() {
+    var config = { timeout: 10000 };
+    var proxyHost = process.env.PROXY_HOST;
+    var proxyPort = process.env.PROXY_PORT;
+    if (proxyHost && proxyPort) {
+          config.proxy = {
+                  protocol: process.env.PROXY_PROTOCOL || 'http',
+                  host: proxyHost,
+                  port: parseInt(proxyPort),
+          };
+          if (process.env.PROXY_USER && process.env.PROXY_PASS) {
+                  config.proxy.auth = {
+                            username: process.env.PROXY_USER,
+                            password: process.env.PROXY_PASS,
+                  };
+          }
+          console.log('[PROXY] Usando proxy: ' + proxyHost + ':' + proxyPort);
+    }
+    return config;
+}
+
+const http = axios.create(buildAxiosConfig());
 
 function sign(timestamp, params) {
-  const paramStr = timestamp + BYBIT_API_KEY + '5000' + params;
-  return crypto.createHmac('sha256', BYBIT_API_SECRET).update(paramStr).digest('hex');
+    const paramStr = timestamp + BYBIT_API_KEY + '5000' + params;
+    return crypto.createHmac('sha256', BYBIT_API_SECRET).update(paramStr).digest('hex');
 }
 
 function buildHeaders(timestamp, signature) {
-  return {
-    'X-BAPI-API-KEY': BYBIT_API_KEY,
-    'X-BAPI-TIMESTAMP': timestamp.toString(),
-    'X-BAPI-RECV-WINDOW': '5000',
-    'X-BAPI-SIGN': signature,
-    'Content-Type': 'application/json',
-  };
+    return {
+          'X-BAPI-API-KEY': BYBIT_API_KEY,
+          'X-BAPI-TIMESTAMP': timestamp.toString(),
+          'X-BAPI-RECV-WINDOW': '5000',
+          'X-BAPI-SIGN': signature,
+          'Content-Type': 'application/json',
+    };
 }
 
 async function withRetry(fn, tentativas) {
-  tentativas = tentativas || 3;
-  for (var i = 0; i < tentativas; i++) {
-    try {
-      return await fn();
-    } catch (err) {
-      if (i === tentativas - 1) throw err;
-      console.warn('[RETRY] Tentativa ' + (i + 1) + ' falhou: ' + err.message + '. Tentando em 2s...');
-      await new Promise(function(r) { setTimeout(r, 2000); });
+    tentativas = tentativas || 3;
+    for (var i = 0; i < tentativas; i++) {
+          try {
+                  return await fn();
+          } catch (err) {
+                  if (i === tentativas - 1) throw err;
+                  console.warn('[RETRY] Tentativa ' + (i + 1) + ' falhou: ' + err.message + '. Tentando em 2s...');
+                  await new Promise(function(r) { setTimeout(r, 2000); });
+          }
     }
-  }
 }
 
 async function publicGet(endpoint, params) {
-  params = params || {};
-  const qs = new URLSearchParams(params).toString();
-  return withRetry(function() {
-    return http.get(BYBIT_BASE_URL + endpoint + (qs ? '?' + qs : ''))
-      .then(function(r) {
-        if (r.data.retCode !== undefined && r.data.retCode !== 0)
-          throw new Error('ByBit [' + r.data.retCode + ']: ' + r.data.retMsg);
-        return r.data;
-      });
-  });
+    params = params || {};
+    const qs = new URLSearchParams(params).toString();
+    return withRetry(function() {
+          return http.get(BYBIT_BASE_URL + endpoint + (qs ? '?' + qs : ''))
+            .then(function(r) {
+                      if (r.data.retCode !== undefined && r.data.retCode !== 0)
+                                  throw new Error('ByBit [' + r.data.retCode + ']: ' + r.data.retMsg);
+                      return r.data;
+            });
+    });
 }
 
 async function privateGet(endpoint, params) {
-  params = params || {};
-  const timestamp = Date.now();
-  const qs = new URLSearchParams(params).toString();
-  const signature = sign(timestamp, qs);
-  const headers = buildHeaders(timestamp, signature);
-  return withRetry(function() {
-    return http.get(BYBIT_BASE_URL + endpoint + (qs ? '?' + qs : ''), { headers })
-      .then(function(r) {
-        if (r.data.retCode !== undefined && r.data.retCode !== 0)
-          throw new Error('ByBit [' + r.data.retCode + ']: ' + r.data.retMsg);
-        return r.data;
-      });
-  });
+    params = params || {};
+    const timestamp = Date.now();
+    const qs = new URLSearchParams(params).toString();
+    const signature = sign(timestamp, qs);
+    const headers = buildHeaders(timestamp, signature);
+    return withRetry(function() {
+          return http.get(BYBIT_BASE_URL + endpoint + (qs ? '?' + qs : ''), { headers })
+            .then(function(r) {
+                      if (r.data.retCode !== undefined && r.data.retCode !== 0)
+                                  throw new Error('ByBit [' + r.data.retCode + ']: ' + r.data.retMsg);
+                      return r.data;
+            });
+    });
 }
 
 async function privatePost(endpoint, body) {
-  body = body || {};
-  const timestamp = Date.now();
-  const bodyStr = JSON.stringify(body);
-  const signature = sign(timestamp, bodyStr);
-  const headers = buildHeaders(timestamp, signature);
-  return withRetry(function() {
-    return http.post(BYBIT_BASE_URL + endpoint, body, { headers })
-      .then(function(r) {
-        if (r.data.retCode !== undefined && r.data.retCode !== 0)
-          throw new Error('ByBit [' + r.data.retCode + ']: ' + r.data.retMsg);
-        return r.data;
-      });
-  });
+    body = body || {};
+    const timestamp = Date.now();
+    const bodyStr = JSON.stringify(body);
+    const signature = sign(timestamp, bodyStr);
+    const headers = buildHeaders(timestamp, signature);
+    return withRetry(function() {
+          return http.post(BYBIT_BASE_URL + endpoint, body, { headers })
+            .then(function(r) {
+                      if (r.data.retCode !== undefined && r.data.retCode !== 0)
+                                  throw new Error('ByBit [' + r.data.retCode + ']: ' + r.data.retMsg);
+                      return r.data;
+            });
+    });
 }
 
 async function validateConnection() {
-  try {
-    const t = await publicGet('/v5/market/time');
-    console.log('ByBit Futures OK. Server time: ' + new Date(parseInt(t.result.timeSecond) * 1000).toISOString());
-    return true;
-  } catch (err) {
-    console.error('Falha ByBit Futures:', err.message);
-    return false;
-  }
+    try {
+          const t = await publicGet('/v5/market/time');
+          console.log('ByBit Futures OK. Server time: ' + new Date(parseInt(t.result.timeSecond) * 1000).toISOString());
+          return true;
+    } catch (err) {
+          console.error('Falha ByBit Futures:', err.message);
+          return false;
+    }
 }
 
 module.exports = { publicGet, privateGet, privatePost, validateConnection };
